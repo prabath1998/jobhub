76e6a4a0cbe51327b2588a8b43d6b59926f4d561
prabath1998 <prabhathudayanga1998@gmail.com>
2025-10-06 22:35:58 +0530

    create onboarding flow

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -2,8 +2,5 @@ import { Navbar } from "@/components/general/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-    </div>
-  );
+  return <div className="grid grid-cols-3 gap-8"></div>;
 }
